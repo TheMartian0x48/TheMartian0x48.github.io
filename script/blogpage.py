c725632f33707a3426208237d657bb44b2b8daf0
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2021-03-14 09:48:15 +0530

    blog page generator

---
+++ script/blogpage.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment, FileSystemLoader
+import json
+
+template_env = Environment(loader=FileSystemLoader(searchpath='./'))
+template = template_env.get_template('blogtemplate.html')
+
+f = json.load(open("bloglist.json"))
+
+with open('../page/blog.html', 'w') as output_file:
+  output_file.write(
+    template.render(
+      blogs=f["blog"]
+    )
+  )
