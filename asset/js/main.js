1f45625970cd154cfd90faeac4c422678f513780
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2021-06-01 08:57:56 +0530

    clean

--- asset/js/main.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-  document.getElementById("navigation").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("navigation").style.width = "0%";
-}
--- blog/project-euler/problem-group-1/problem16.py
+++
@@ -1,5 +0,0 @@
-from math import pow
-
-n = int(pow(2, 1000))
-res = sum([int(ch) for ch in str(n)])
-print(res)
